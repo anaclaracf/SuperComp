399eda65277951389a5b57f6e47a244a67625b86
anaccf5 <anaclara.2001@hotmail.com>
2022-03-16 11:53:47 -0300

    algoritmo sm implementado

--- projeto.cpp
+++ projeto.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include <cmath>
 
 using namespace std;
+typedef vector<vector<int>> matrix;
 
 int calculate_score(char a, char b);
+matrix smithWaterman_Algorithm(matrix H, string first_seq, string second_seq, int n, int m, int w);
 
 int calculate_score(char a, char b){
     if(a == b){
@@ -14,38 +17,73 @@ int calculate_score(char a, char b){
     }
 }
 
+matrix smithWaterman_Algorithm(matrix H, string first_seq, string second_seq, int n, int m, int w){
+    int diagonal, deletion, insertion;
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            w = calculate_score(first_seq[i], second_seq[j]);
+            diagonal = H[i-1][j-1] + w;
+            deletion = H[i-1][j] - 1;
+            insertion = H[i][j-1] - 1;
+
+            // cout << diagonal << endl;
+            // cout << deletion << endl;
+            // cout << insertion << endl;
+
+            // cout << max({0, diagonal, deletion, insertion}) << endl;
+
+            H[i][j] = max({0, diagonal, deletion, insertion});
+        }
+    }
+
+    return H;
+}
+
 
 int main(){
 
     int n, m;
-    string first_sequence, second_sequence;
+    int w = 0;
+    string first_seq, second_seq;
     char first_signal = '-';
 
     cin >> n >> m;
-    cin >> first_sequence >> second_sequence;
+    cin >> first_seq >> second_seq;
 
-    first_sequence = first_signal + first_sequence;
-    second_sequence = first_signal + second_sequence;
 
-    // cout << n << endl << m << endl;
-    // cout << first_sequence << endl << second_sequence << endl;
+    // fromatando as sequencias de DNA
+    first_seq = first_signal + first_seq;
+    second_seq = first_signal + second_seq;
 
-    int matriz[n+1][m+1] = {};
+    // printando as entradas geradas pelo arquivo de entrada
+    cout << n << endl << m << endl;
+    cout << first_seq << endl << second_seq << endl;
 
-    // for (int i = 0; i < n; i++){
-    //     for (int j = 0; j < m; j++){
-    //         cout << matriz[i][j] << "  ";
+    // criando a matriz e iniciando ela com zeros
+    matrix H;
+    H.resize(n+1);
+    for(int e=0; e<=n; e++){
+        H[e].resize(m+1);
+    }
+
+    // vizualizando a matriz preenchida com zeros
+    // for (int i = 0; i <= n; i++){
+    //     for (int j = 0; j <= m; j++){
+    //         cout << H[i][j] << "  ";
     //     }
     //     cout << "\n ";
     // }
 
-    for (int i = 1; i < n+1; i++){
-        for (int j = 1; j < m+1; j++){
+    // aplicando o algoritmo de smith waterman
+    H = smithWaterman_Algorithm(H, first_seq, second_seq, n, m, w);
 
-            // vendo se é match
-            // if()
-
+    // visualizando a matriz com o algoritmo aplicado
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= m; j++){
+            cout << H[i][j] << "  ";
         }
+        cout << "\n ";
     }
 
     return 0;

f4dda28df74d706101c6c0f75384406f55500f0f
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2022-03-29 23:05:15 +0000

    busca logac

--- BuscaLocal/main.cpp
+++ BuscaLocal/main.cpp
@@ -25,9 +25,7 @@ int calculate_score(char a, char b){
 
 matrix max_score(matrix H, string first_seq, string second_seq, int k, int w){
     int diagonal, deletion, insertion;
-    int valor_max = 0;
     string seq_max;
-    int numero = 0;
 
     for (int i = 1; i <= k; i++){
         for (int j = 1; j <= k; j++){
@@ -54,7 +52,7 @@ int main(){
     string new_seqA, new_seqB;
     string sub_seqA, sub_seqB;
 
-    vector<max_value> valores_max;
+    
     
     char first_signal = '-';
 
@@ -71,81 +69,91 @@ int main(){
 
     default_random_engine generator;
 
-    // calculando o k aleatório
-    uniform_int_distribution<int> k_aleatorio(1, max_k);
-    // k = k_aleatorio(generator);
-    k = 6;
+    for(int g=0; g < 10; g++){
 
-    // calculando o j aleatorio
-    uniform_int_distribution<int> j_aleatorio(0, m - k);
-    j = j_aleatorio(generator);
+        vector<max_value> valores_max;
 
-    // calculando o i aleatorio 
-    uniform_int_distribution<int> i_aleatorio(0, n - k);
-    
-    cout << "k = " << k << endl;
-    cout << "j = " << j << endl;
-    
-    // gerando a subsequencia de b
-    sub_seqB = seqB.substr(j, k);
+        // calculando o k aleatório
+        uniform_int_distribution<int> k_aleatorio(1, max_k);
+        k = k_aleatorio(generator);
+        // k = 5;
 
-    // cout << seqB << endl;
-    // cout << sub_seqB << endl;
+        // calculando o j aleatorio
+        uniform_int_distribution<int> j_aleatorio(0, m - k);
+        j = j_aleatorio(generator);
 
-    // gerando um número aleatorio para p
-    uniform_int_distribution<int> p_aleatorio(1, 100);
-    p = p_aleatorio(generator);
-
-    cout << "p = " << p << endl;
-
-    // criando matriz para calculo do max score
-    new_seqB = first_signal + sub_seqB;
-    matrix H;
-    H.resize(k+1);
-    for(int e=0; e<=k; e++){
-        H[e].resize(k+1);
-    }
-
-    // gerando a subsequencia de a
-    for(int e = 0; e < 5; e++){
+        // calculando o i aleatorio 
+        uniform_int_distribution<int> i_aleatorio(0, n - k);
         
-        max_value highScore;
+        // cout << "k = " << k << endl;
+        // cout << "j = " << j << endl;
+        
+        // gerando a subsequencia de b
+        sub_seqB = seqB.substr(j, k);
 
-        i = i_aleatorio(generator);
-        sub_seqA = seqA.substr(i, k);
-        new_seqA = first_signal + sub_seqA;
+        // gerando um número aleatorio para p
+        uniform_int_distribution<int> p_aleatorio(1, 100);
+        p = p_aleatorio(generator);
 
-        // tie(highScore.score_max, highScore.seq) = max_score(H, new_seqA, new_seqB, k, w);
-        H = max_score(H, new_seqA, new_seqB, k, w);
-        int max = 0;
+        // cout << "p = " << p << endl;
 
-
-        for (i = 0; i < k; i++){
-            for (j = 0; j < k; j++){
-                cout << H[i][j] << "  ";
-                if(H[i][j] > max){
-                    max = H[i][j];
-                }
-            }
-            cout << "\n ";
+        // criando matriz para calculo do max score
+        new_seqB = first_signal + sub_seqB;
+        matrix H;
+        H.resize(k+1);
+        for(int e=0; e<=k; e++){
+            H[e].resize(k+1);
         }
+
+        // gerando a subsequencia de a
+        for(int e = 0; e < p; e++){
+            
+            max_value highScore;
+
+            i = i_aleatorio(generator);
+            sub_seqA = seqA.substr(i, k);
+            new_seqA = first_signal + sub_seqA;
+
+            H = max_score(H, new_seqA, new_seqB, k, w);
+            int max = 0;
+
+            for (i = 0; i < k; i++){
+                for (j = 0; j < k; j++){
+                    // cout << H[i][j] << "  ";
+                    if(H[i][j] > max){
+                        max = H[i][j];
+                    }
+                }
+                // cout << endl;
+            }
+
+            // cout << endl;
+            highScore.score_max = max;
+            highScore.seq = sub_seqA;
+            valores_max.push_back(highScore);
         
-        cout << max << endl;
-        cout << sub_seqA << endl;
-        cout << "\n ";
+        }
 
+        int tamanho_vetor = valores_max.size();
+        int score_max = 0;
+        string seq_final;
 
+        for (int i = 0; i < tamanho_vetor; i++){
+            if(valores_max[i].score_max > score_max){
+                score_max = valores_max[i].score_max;
+                seq_final = valores_max[i].seq;
+                // cout << seq_final;
+            }
+        } 
 
-        // cout << highScore.score_max << endl;
-        // cout << highScore.seq << endl;
-        // valores_max.push_back(highScore);
+        cout << "max score: " << score_max << endl;
+        cout << "sub sequencia A utilizada: " << seq_final << endl;
+        cout << "sub sequencia B utilizada: " << sub_seqB << endl;
+        cout << endl;
     }
 
-    // for (int i = 0; i < sizeof(valores_max); i++){
-    //     cout << valores_max[i].score_max << endl;
-    //     cout << valores_max[i].seq << endl;
-    // } 
 
+    
 
 
     return 0;

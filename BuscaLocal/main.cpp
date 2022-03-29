f027e2a12b832e45bfb14aa989f01d830130a114
anaccf5 <anaclara.2001@hotmail.com>
2022-03-28 23:02:19 -0300

    projeto 2

---
+++ BuscaLocal/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <cmath>
+#include <tuple>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+    int n, m;
+    string first_seq, second_seq;
+
+    cin >> n >> m;
+    cin >> first_seq >> second_seq;
+
+    
+
+    return 0;
+}
